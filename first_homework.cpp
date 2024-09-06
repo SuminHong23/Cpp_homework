8b2ed32ba784f0caf2a3d12d3e9de3d5050d1647
SuminHong23 <hongsumin0909@gmail.com>
2024-09-06 15:55:00 +0900

    cirst commit

---
+++ first_homework.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    const char* firstFile = "C:\\c++\\numbers.txt";
+    fstream fin(firstFile, ios::in);
+    if (!fin) {
+        cout << firstFile << "열기 오류";
+        return 0;
+    }
+    int d, sum = 0, count = 0;
+    double avg = 0;
+    while (!fin.eof()) {
+        fin >> d;
+        sum += d;
+        count++;
+    }
+    avg = (double)sum / count;
+    cout << "count=" << count << " average=" << avg << endl;
+    fin.close();
+    
+
+}
